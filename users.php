00558a73d967b6b687ad94a15ac86f8cd81ffd6f
Nomakram1 <naumanakram21@gmail.com>
2022-09-25 23:33:17 +0500

    Create users.php

---
+++ users.php
@@ -0,0 +1,61 @@
+<?
+
+private function validateUser($user){
+  if($user['name'] && $user['login'] && $user['email'] && $user['password'] && strlen($user['name']) >= 10)
+    return true;
+  return false;
+}
+
+public function updateUsers($users)
+{
+	foreach ($users as $user) {
+		try {
+			if (validateUser($user))
+				DB::table('users')->where('id', $user['id'])->update([
+					'name' => $user['name'],
+					'login' => $user['login'],
+					'email' => $user['email'],
+					'password' => md5($user['password'])
+				]);
+		} catch (\Throwable $e) {
+			return Redirect::back()->withErrors(['error', ['We couldn\'t update user: ' . $e->getMessage()]]);
+		}
+	}
+	return Redirect::back()->with(['success', 'All users updated.']);
+}
+
+public function storeUsers($users)
+{
+
+    foreach ($users as $user) {
+        try {
+			if (validateUser($user))
+				DB::table('users')->insert([
+					'name' => $user['name'],
+					'login' => $user['login'],
+					'email' => $user['email'],
+					'password' => md5($user['password'])
+            ]);
+        } catch (\Throwable $e) {
+            return Redirect::back()->withErrors(['error', ['We couldn\'t store user: ' . $e->getMessage()]]);
+        }
+    }
+    $this->sendEmail($users);
+    return Redirect::back()->with(['success', 'All users created.']);
+}
+
+private function sendEmail($users)
+{
+    foreach ($users as $user) {
+        $message = 'Account has beed created. You can log in as <b>' . $user['login'] . '</b>';
+        if ($user['email']) {
+            Mail::to($user['email'])
+                ->cc('support@company.com')
+                ->subject('New account created')
+                ->queue($message);
+        }
+    }
+    return true;
+}
+
+?>
